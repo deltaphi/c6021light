134fedd01d3795f252d2fa4812a412f53b0af3fd
deltaphi <damian.philipp@gmx.net>
2020-06-21 21:45:01 +0200

    Added code for automatic release builds with Github Actions

---
+++ .github/copy-release-files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OUTPUT_GLOB=".pio/build/**/firmware.*"
+
+DESTINATION_DIR=".github/releasefiles"
+
+# Create destination directory
+mkdir -p "$DESTINATION_DIR"
+
+# Copy all files to destination directory
+for f in $OUTPUT_GLOB; do
+    echo "Source: $f"
+    dironly=`dirname $f`
+    target=`basename $dironly`
+    filename=`basename $f`
+    destination="$DESTINATION_DIR/$target-$filename"
+    echo "Destionation: $destination"
+    cp "$f" "$destination"
+done
