475226b416313f2affc1bbcfc0b64c558ab265b6
Damian Philipp <damian.philipp@gmx.net>
2020-08-02 13:09:10 +0200

    Reduced Heap size.

--- c6021light/include/FreeRTOSConfig.h
+++ c6021light/include/FreeRTOSConfig.h
@@ -9,7 +9,7 @@
 #define configMAX_PRIORITIES      ( 5 )
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY      ( 5 )
 #define configMINIMAL_STACK_SIZE  ( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE     ( ( size_t ) ( 17 * 1024 ) )
+#define configTOTAL_HEAP_SIZE     ( ( size_t ) ( 5 * 1024 ) )
 #define configMAX_TASK_NAME_LEN   ( 16 )
 #define configUSE_TRACE_FACILITY  0
 #define configUSE_16_BIT_TICKS    0
