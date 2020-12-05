884e81702cbc878b13c05bc4594deda899eb2eb0
Damian Philipp <damian.philipp@gmx.net>
2020-12-05 20:29:07 +0100

    Roughly working LN RX.

--- c6021light/src/hal/LibOpencm3Hal.cpp
+++ c6021light/src/hal/LibOpencm3Hal.cpp
@@ -7,6 +7,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/rtc.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/timer.h>
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -22,6 +23,8 @@
 #include "eeConfig.h"
 
 #include "FreeRTOS.h"
+
+#include <LocoNet.h>
 
 extern "C" {
 /* _write code taken from example at
@@ -49,8 +52,6 @@ namespace hal {
 freertossupport::OsQueue<LibOpencm3Hal::CanMsg> LibOpencm3Hal::canrxq;
 TaskHandle_t LibOpencm3Hal::taskToNotify;
 
-static uint32_t lnBitTime = ((rcc_apb1_frequency * 2 * 4) / 16666) + 250;
-
 void LibOpencm3Hal::beginClock() {
   // Enable the overall clock.
   rcc_clock_setup_in_hse_8mhz_out_72mhz();
@@ -76,10 +77,6 @@ void LibOpencm3Hal::beginGpio() {
   gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_OPENDRAIN, GPIO13);
   gpio_set(GPIOC, GPIO13);  // Turn the LED off.
 
-  gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_OPENDRAIN, GPIO15);  // LN TX
-  gpio_set(GPIOB, GPIO15);  // Set Idle High (TODO: Correct?)
-  gpio_set_mode(GPIOB, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO14);  // LN RX
-
   gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_OPENDRAIN, GPIO0);  // Extra LED
   gpio_set(GPIOA, GPIO0);  // Set Idle High (TODO: Correct?)
 
@@ -89,57 +86,7 @@ void LibOpencm3Hal::beginGpio() {
 }
 
 void LibOpencm3Hal::beginLocoNet() {
-  // Setup Timer
-  /* Enable TIM2 clock. */
-	rcc_periph_clock_enable(RCC_TIM2);
-
-	/* Enable TIM2 interrupt. */
-	nvic_enable_irq(NVIC_TIM2_IRQ);
-
-	/* Reset TIM2 peripheral to defaults. */
-	rcc_periph_reset_pulse(RST_TIM2);
-
-	/* Timer global mode:
-	 * - No divider
-	 * - Alignment edge
-	 * - Direction up
-	 * (These are actually default values after reset above, so this call
-	 * is strictly unnecessary, but demos the api for alternative settings)
-	 */
-	timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT,
-		TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-
-	/*
-	 * Please take note that the clock source for STM32 timers
-	 * might not be the raw APB1/APB2 clocks.  In various conditions they
-	 * are doubled.  See the Reference Manual for full details!
-	 * In our case, TIM2 on APB1 is running at double frequency, so this
-	 * sets the prescaler to have the timer run at 5kHz
-	 */
-	//timer_set_prescaler(TIM2, ((rcc_apb1_frequency * 2) / 5000));
-
-	/* Disable preload. */
-	timer_disable_preload(TIM2);
-	timer_continuous_mode(TIM2);
-
-	/* count full range, as we'll update compare value continuously */
-	timer_set_period(TIM2, 65535);
-
-	/* Set the initual output compare value for OC1. */
-	//timer_set_oc_value(TIM2, TIM_OC1, lnBitTime);
-
-	/* Counter enable. */
-	timer_enable_counter(TIM2);
-
-	/* Enable Channel 1 compare interrupt to recalculate compare values */
-	//timer_enable_irq(TIM2, TIM_DIER_CC1IE);
-  
-  // Setup level change interrupt
-  exti_select_source(EXTI14, GPIOB);
-  exti_set_trigger(EXTI14, EXTI_TRIGGER_FALLING);  // Experiment: Use both
-  exti_enable_request(EXTI14);
-  nvic_enable_irq(NVIC_EXTI15_10_IRQ);
-  // TODO: IRQ priority?
+  LocoNet.init(15);
 }
 
 void LibOpencm3Hal::beginSerial() {
@@ -197,83 +144,6 @@ void LibOpencm3Hal::beginCan() {
 
 extern "C" {
 
-static uint32_t rxBitCount = 0;
-constexpr static const uint32_t maxRxBitCount = 9;
-static uint32_t rxByte = 0;
-
-constexpr static const uint32_t rxArrayLength = 256;
-static uint32_t rxArray[rxArrayLength];
-static uint32_t rxArrayIndex = 0;
-
-constexpr static const uint32_t txByteConst = 176;
-static uint32_t txByte = 176;
-
-void exti15_10_isr(void) {
-  // Check if it really was EXTI14 that triggered this interrupt.
-  if (exti_get_flag_status(EXTI14)) {  // TODO: Is the logic the right way around?
-    uint16_t compare_time = timer_get_counter(TIM2);
-    gpio_clear(GPIOA, GPIO5);
-		uint16_t new_time = compare_time + (lnBitTime / 2);
-    /* Set the initual output compare value for OC1. */
-    timer_set_oc_value(TIM2, TIM_OC1, new_time);
-
-    /* Enable Channel 1 compare interrupt to recalculate compare values */
-		timer_clear_flag(TIM2, TIM_SR_CC1IF);
-    timer_enable_irq(TIM2, TIM_DIER_CC1IE);
-    rxBitCount = 0;
-    rxByte = 0;
-
-    exti_disable_request(EXTI14);
-    exti_reset_request(EXTI14);
-  }
-  exti_reset_request(EXTI14);
-}
-
-
-void tim2_isr(void) {
-  if (timer_get_flag(TIM2, TIM_SR_CC1IF)) {
-
-    gpio_toggle(GPIOA, GPIO6);
-
-		/* Clear compare interrupt flag. */
-		timer_clear_flag(TIM2, TIM_SR_CC1IF);
-
-		uint16_t compare_time = timer_get_counter(TIM2);
-		uint16_t new_time = compare_time + lnBitTime;
-    /* Set the initual output compare value for OC1. */
-    timer_set_oc_value(TIM2, TIM_OC1, new_time);
-
-    uint32_t lnRxBit = gpio_get(GPIOB, GPIO14);
-    if (rxBitCount < maxRxBitCount) {     
-      if (rxBitCount > 0 || rxBitCount < maxRxBitCount - 1) {
-        rxByte >>= 1;
-        if (lnRxBit != 0) {
-          rxByte |= 0x80;
-          gpio_set(GPIOA, GPIO4);
-        } else {
-          gpio_clear(GPIOA, GPIO4);
-        }
-      }
-      ++rxBitCount;
-    } else {
-      if (rxByte != 0) {
-        gpio_clear(GPIOA, GPIO4);
-      } else {
-        gpio_set(GPIOA, GPIO4);
-      }
-      rxArray[rxArrayIndex] = rxByte;
-      ++rxArrayIndex;
-      gpio_set(GPIOA, GPIO5);
-      timer_disable_irq(TIM2, TIM_DIER_CC1IE);
-      exti_reset_request(EXTI14);
-      exti_enable_request(EXTI14);
-    }
-
-		/* Toggle LED to indicate compare event. */
-		//gpio_toggle(LED1_PORT, LED1_PIN);
-	}
-}
-
 void usb_lp_can_rx0_isr(void) {
   BaseType_t anyTaskWoken = pdFALSE;
   for (uint32_t messageCount = CAN_RF0R(CAN1) & 3; messageCount > 0; --messageCount) {
@@ -299,8 +169,7 @@ void usb_lp_can_rx0_isr(void) {
     }
   }
   BaseType_t taskWoken;
-  BaseType_t notifyResult =
-      xTaskNotifyFromISR(LibOpencm3Hal::taskToNotify, 1, eSetValueWithoutOverwrite, &taskWoken);
+  xTaskNotifyFromISR(LibOpencm3Hal::taskToNotify, 1, eSetValueWithoutOverwrite, &taskWoken);
 
   if (anyTaskWoken == pdTRUE || taskWoken == pdTRUE) {
     taskYIELD();
--- c6021light/src/tasks/ConsoleTask/ConsoleTask.cpp
+++ c6021light/src/tasks/ConsoleTask/ConsoleTask.cpp
@@ -1,13 +1,27 @@
 #include "tasks/ConsoleTask/ConsoleTask.h"
 
+#include <LocoNet.h>
+#include <cstdio>
+
 namespace tasks {
 namespace ConsoleTask {
 
 void ConsoleTask::TaskMain() {
   while (1) {
     halImpl_->loopSerial();
+
+    lnMsg * LnPacket = LocoNet.receive();
+    if (LnPacket) {
+      printf("LN RX: ");
+      for (int i = 0; i < getLnMsgSize(LnPacket); ++i) {
+        printf(" %x", LnPacket->data[i]);
+      }
+      printf("\n");
+    }
+
   }
 }
+
 
 }  // namespace ConsoleTask
 }  // namespace tasks
