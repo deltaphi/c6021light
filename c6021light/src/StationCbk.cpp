1f2a15b016f4a5f550cfe00d436460c98483e917
Damian Philipp <damian.philipp@gmx.net>
2020-12-07 22:30:11 +0100

    Forward Stop/Go from CAN to LocoNet

--- c6021light/src/StationCbk.cpp
+++ c6021light/src/StationCbk.cpp
@@ -1,5 +1,7 @@
 #include "StationCbk.h"
 
+#include "LocoNet.h"
+
 #include "tasks/RoutingTask/RoutingTask.h"
 
 void AccessoryCbk::begin(hal::HalBase& hal) { this->hal_ = &hal; }
@@ -7,4 +9,7 @@ void AccessoryCbk::begin(hal::HalBase& hal) { this->hal_ = &hal; }
 void AccessoryCbk::setSystemState(bool onOff, bool response) {
   // When the system is stopped, turn on the LED.
   hal_->led(!onOff);
+  if (!response) {
+    LocoNet.reportPower(onOff);
+  }
 }
