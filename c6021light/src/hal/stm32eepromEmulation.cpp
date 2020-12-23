b824622543d0e82a40366a664458d6ecfbf2f646
Damian Philipp <damian.philipp@gmx.net>
2020-12-23 11:46:49 +0100

    Converted from nimaltd's EE to FlashFairyPP for EEPROM emulation.

--- c6021light/src/hal/stm32eepromEmulation.cpp
+++ c6021light/src/hal/stm32eepromEmulation.cpp
@@ -1,42 +1,48 @@
 #include "hal/stm32eepromEmulation.h"
 
 #include <cstdio>
+#include <type_traits>
 
-#include "ee.h"
-#include "eeConfig.h"
+#include <libopencm3/stm32/flash.h>
+
+#include "FlashFairyPP/FlashFairyPP.h"
 
 namespace hal {
 
-void beginEE() { ee_init(); }
+// Extern declarations for flash pages defined in linker script
+extern "C" {
+  extern uint32_t _flashFairyPage0;
+  extern uint32_t _flashFairyPage1;
+}
+
+FlashFairyPP::FlashFairyPP flashFairy;
+
+void beginEE() {
+  FlashFairyPP::FlashFairyPP::Config_t config;
+  config.pages[0] = &_flashFairyPage0;
+  config.pages[1] = &_flashFairyPage1;
+
+  flashFairy.Init(config);
+}
 
 DataModel LoadConfig() {
   DataModel model;
-  ee_read(DataAddresses::accessoryRailProtocol, sizeof(DataModel::accessoryRailProtocol),
-          reinterpret_cast<uint8_t*>(&model.accessoryRailProtocol));
+  model.accessoryRailProtocol = static_cast<RR32Can::RailProtocol>(flashFairy.getValue(DataAddresses::accessoryRailProtocol));
   return model;
 }
 
 void SaveConfig(const DataModel& model) {
-  ee_writeToRam(DataAddresses::accessoryRailProtocol, sizeof(DataModel::accessoryRailProtocol),
-                reinterpret_cast<uint8_t*>(&const_cast<DataModel&>(model).accessoryRailProtocol));
-  ee_commit();
+  uint16_t value = static_cast<std::underlying_type<RR32Can::RailProtocol>::type>(model.accessoryRailProtocol);
+  flashFairy.setValue(DataAddresses::accessoryRailProtocol, value);
 }
 
-extern "C" uint8_t HAL_FLASHEx_Erase(FLASH_EraseInitTypeDef* addr, uint32_t* error) {
-  if (addr->NbPages == 1) {
-    flash_erase_page(addr->PageAddress);
-    *error = flash_get_status_flags();
-    if (*error != FLASH_SR_EOP) {
-      return HAL_NOK;
-    } else {
-      *error = 0xFFFFFFFF;  // Expected value by caller of OK case.
-      return HAL_OK;
-    }
-  } else {
-    printf("HAL_FLASHEx_Erase: requested erase not supported.\n");
-    return HAL_NOK;
-  }
+extern "C" void flash_write(uint32_t *pagePtr, uint32_t line) {
+  flash_program_half_word(reinterpret_cast<uint32_t>(pagePtr) + 2, line >> 16);
+  flash_program_half_word(reinterpret_cast<uint32_t>(pagePtr), line);
 }
+
+}  // namespace hal
+
 
 extern "C" uint8_t HAL_FLASH_Program(uint8_t flashProgramType, uint32_t addr, uint64_t data) {
   if (flashProgramType == FLASH_TYPEPROGRAM_HALFWORD) {
