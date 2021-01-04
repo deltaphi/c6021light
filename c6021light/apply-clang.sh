c72106c8f6b71f792ef72183de71ad8826176da1
Damian Philipp <damian.philipp@gmx.net>
2021-01-04 08:29:02 +0100

    Add support for desktop-based unit testing

--- c6021light/apply-clang.sh
+++ c6021light/apply-clang.sh
@@ -4,7 +4,7 @@ PATH=$PATH:"/c/Program Files/LLVM/bin/":"/d/Program Files/LLVM/bin/"
 
 CLANG_BINARY="clang-format.exe"
 
-for filename in `find src -iname "*.cpp" -o -iname "*.h"`
+for filename in `find src test -iname "*.cpp" -o -iname "*.h"`
 do
     echo "Formatting $filename"
     "$CLANG_BINARY" -i "$filename"
---
+++ c6021light/test/EmptyTest.cpp
@@ -0,0 +1,4 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(EmptyTest, DummyTest) { EXPECT_TRUE(true); }
