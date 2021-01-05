da65d7af13edf9d93cd1ebf9844f91489b0bb6ac
Damian Philipp <damian.philipp@gmx.net>
2021-01-05 17:24:04 +0100

    Fix unit tests

--- c6021light/test/mocks/RoutingForwarderMock.h
+++ c6021light/test/mocks/RoutingForwarderMock.h
@@ -16,8 +16,7 @@ class RoutingForwarderMock : public tasks::RoutingTask::RoutingForwarder {
  public:
   MOCK_METHOD(void, forwardLocoChange, (const RR32Can::LocomotiveData& loco, LocoDiff_t& diff),
               (override));
-  MOCK_METHOD(void, forward, (const RR32Can::Identifier rr32id, const RR32Can::Data& rr32data),
-              (override));
+  MOCK_METHOD(void, forward, (const RR32Can::CanFrame& frame), (override));
 };
 
 }  // namespace RoutingTask
