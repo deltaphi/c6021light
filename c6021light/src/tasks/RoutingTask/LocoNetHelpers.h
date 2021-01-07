c716a5c5974a395a563a74eb25a312debbcda2e6
Damian Philipp <damian.philipp@gmx.net>
2021-01-07 21:22:15 +0100

    Add more constexprs

--- c6021light/src/tasks/RoutingTask/LocoNetHelpers.h
+++ c6021light/src/tasks/RoutingTask/LocoNetHelpers.h
@@ -16,11 +16,11 @@ constexpr static const uint8_t kFunctionsInDirfMessage = 5;
 constexpr static const uint8_t kFunctionsInSndMessage = 4;
 constexpr static const uint8_t kLowestFunctionInSndMessage = 5;
 
-inline RR32Can::Velocity_t lnSpeedToCanVelocity(RR32Can::Velocity_t speed) {
+constexpr RR32Can::Velocity_t lnSpeedToCanVelocity(RR32Can::Velocity_t speed) {
   return (speed * RR32Can::kMaxEngineVelocity / kLocoNetMaxVeloctiy);
 }
 
-inline RR32Can::Velocity_t canVelocityToLnSpeed(RR32Can::Velocity_t velocity) {
+constexpr RR32Can::Velocity_t canVelocityToLnSpeed(RR32Can::Velocity_t velocity) {
   return (velocity * kLocoNetMaxVeloctiy) / RR32Can::kMaxEngineVelocity;
 }
 
@@ -30,20 +30,21 @@ uint8_t locoToDirf(const RR32Can::LocomotiveData& loco);
 void sndToLoco(const uint8_t snd, RR32Can::LocomotiveData& loco);
 uint8_t locoToSnd(const RR32Can::LocomotiveData& loco);
 
-inline RR32Can::Locomotive::Address_t getLocoAddress(const rwSlotDataMsg& slotRead) {
+constexpr RR32Can::Locomotive::Address_t getLocoAddress(const rwSlotDataMsg& slotRead) {
   RR32Can::Locomotive::Address_t::value_type address = slotRead.adr2 << 7;
   address |= slotRead.adr & 0x7F;
   return RR32Can::Locomotive::Address_t{address};
 }
 
-inline void putLocoAddress(rwSlotDataMsg& slotRead, const RR32Can::Locomotive::Address_t address) {
+constexpr void putLocoAddress(rwSlotDataMsg& slotRead,
+                              const RR32Can::Locomotive::Address_t address) {
   slotRead.adr = address.value() & 0x7F;  // Loco Address low bits
   slotRead.adr2 = address.value() >> 7;   // Loco Address high bits
 }
 
-inline bool isDispatchGet(const slotMoveMsg& msg) { return msg.src == 0; }
-inline bool isDispatchPut(const slotMoveMsg& msg) { return msg.dest == 0; }
-inline bool isNullMove(const slotMoveMsg& msg) { return msg.src == msg.dest; }
+constexpr bool isDispatchGet(const slotMoveMsg& msg) { return msg.src == 0; }
+constexpr bool isDispatchPut(const slotMoveMsg& msg) { return msg.dest == 0; }
+constexpr bool isNullMove(const slotMoveMsg& msg) { return msg.src == msg.dest; }
 
 }  // namespace RoutingTask
 }  // namespace tasks
