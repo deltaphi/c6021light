daadbc8bdea24ff93d07554947e78fb5ee72a965
Damian Philipp <damian.philipp@gmx.net>
2021-02-21 13:29:02 +0100

    Refactoring of test cases

--- c6021light/test/integration/SlotServerProcessing.cpp
+++ c6021light/test/integration/SlotServerProcessing.cpp
@@ -6,26 +6,8 @@
 #include "mocks/RoutingTaskFixture.h"
 #include "mocks/SequenceMaker.h"
 
-#include "tasks/RoutingTask/LocoNetSlotServer.h"
-
 namespace tasks {
 namespace RoutingTask {
-
-TEST(SlotServer, AllocateAddress_WillReturnInitializedSlot) {
-  const LocoNetSlotServer::LocoAddr_t locoAddr = RR32Can::MachineLocomotiveAddress(50U);
-  LocoNetSlotServer slotServer;
-
-  const LocoNetSlotServer::SlotDB_t::iterator it =
-      slotServer.findOrAllocateSlotForAddress(locoAddr);
-  EXPECT_NE(it, slotServer.end());
-  EXPECT_EQ(it->loco.getAddress(), locoAddr);
-}
-
-TEST(SlotServer, extractLocoAddress) {
-  const auto locoAddr = RR32Can::MachineLocomotiveAddress(50U);
-  const lnMsg LnPacket = Ln_LocoAddr(locoAddr);
-  EXPECT_EQ(locoAddr, LocoNetSlotServer::extractLocoAddress(LnPacket));
-}
 
 using SlotServerFixture = mocks::RoutingTaskFixture;
 
