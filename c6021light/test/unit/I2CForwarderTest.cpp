a393297cff8b019ac5b865db0c848446808069d9
deltaphi <damian.philipp@posteo.de>
2025-02-08 21:46:56 +0100

    Do/Dont Remap

--- c6021light/test/unit/I2CForwarderTest.cpp
+++ c6021light/test/unit/I2CForwarderTest.cpp
@@ -28,3 +28,27 @@ TEST(I2CForwarderTest, Remap_Remapped) {
 
   EXPECT_EQ(actual, expected);
 }
+
+TEST(I2CForwarderTest, Remap_Umapped_Remapped_Unmapped) {
+  auto i2cForwarder = tasks::RoutingTask::I2CForwarder();
+  DataModel model;
+  i2cForwarder.init(model);
+
+  const RR32Can::MachineTurnoutAddress turnoutAddress{0xAAu};
+  const RR32Can::MachineTurnoutAddress remappedAddress{0xBBu};
+
+  {
+    const RR32Can::MachineTurnoutAddress actual{i2cForwarder.remapTurnoutAddress(turnoutAddress)};
+    EXPECT_EQ(actual, turnoutAddress);
+  }
+  model.i2cTurnoutMap.insert({turnoutAddress, remappedAddress});
+  {
+    const RR32Can::MachineTurnoutAddress actual{i2cForwarder.remapTurnoutAddress(turnoutAddress)};
+    EXPECT_EQ(actual, remappedAddress);
+  }
+  model.i2cTurnoutMap.erase(turnoutAddress);
+  {
+    const RR32Can::MachineTurnoutAddress actual{i2cForwarder.remapTurnoutAddress(turnoutAddress)};
+    EXPECT_EQ(actual, turnoutAddress);
+  }
+}
